0979a5ed1c19e9869c78023ef0305edfc1165c22
Arghya003 <arghyaguha105@gmail.com>
2023-08-14 00:12:08 +0530

    second commit

--- todo-backend/controllers/TodoController.js
+++ todo-backend/controllers/TodoController.js
@@ -1,26 +1,68 @@
 const mongoose = require("mongoose");
 
-const Todos= require("../dBTodos")
-
+const Todos = require("../dBTodos");
 
 //getTodo
-const getTodos= async(req,res)=>{
-    try{
-        const allTodos= await Todos.find({}).sort({createdAt:-1})
-        res.status(200).send(allTodos)
-    }
-    catch(err){
-        res.status(400).send(err.message)
-    }
-}
-
+const getTodos = async (req, res) => {
+  try {
+    const allTodos = await Todos.find({}).sort({ createdAt: -1 });
+    res.status(200).send(allTodos);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
 // create a new Todo
-const createTodos = async (req, res) => {};
+const createTodos = async (req, res) => {
+  const dbTodo = req.body;
+  try {
+    const newTodo = await Todos.create(dbTodo);
+    res.status(200).send(newTodo);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
-const updateTodos = async (req, res) => {};
+//update Todo
+const updateTodos = async (req, res) => {
+  const { id } = req.params.id;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send(`There is no todo with ID of ${id}`);
+    }
+    const todoID = { _id: id };
+    const update = { completed: true };
 
-const deleteTodos = async (req, res) => {};
+    const updateTodo = await Todos.findOneAndUpdate(todoID, update);
+    
+    if(!updateTodo){
+        return res.status(400).send(`There is no todo with ID of ${id}`);
+    }
+    
+    res.status(200).send(updateTodo);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+//delte Todo
+const deleteTodos = async (req, res) => {
+  const { id } = req.params.id;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send(`There is no todo with ID of ${id}`);
+    }
+    const todoID = { _id: id };
+  
+
+    const deleteTodo = await Todos.findOneAndDelete(todoID);
+    
+  
+    
+    res.status(200).send(deleteTodoTodo);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 module.exports = {
   getTodos,
--- todo-frontend/src/components/todo/index.js
+++ todo-frontend/src/components/todo/index.js
@@ -1,22 +1,47 @@
-import React, { useState } from 'react'
-import { Container } from './styles';
-import Form from "../form"
-
-
-
-
-
-
-  
+import React, { useEffect, useState } from "react";
+import { Container } from "./styles";
+import Form from "../form";
+import axios from "../../axios";
 
 function Todo() {
   const [input, setInput] = useState("");
-  console.log(input,"input")
+  const [todos, setTodos] = useState([]);
+  console.log(input, "input");
+
+  const fetchData = async () => {
+    try {
+      const reponse = await axios.get("/todos");
+      setTodos(reponse.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addTodo = async (e) => {
+    console.log("added todos");
+    e.preventDefault();
+    if (input.length === 0) return null;
+    await axios.post("/todos", [
+      {
+        ...todos,
+        text: input,
+        completed: false,
+      },
+    ]);
+    fetchData();
+    setInput('')
+  };
+  console.log(todos, "todos");
+
   return (
     <Container>
       <h3>Work Left</h3>
       {/* Form*/}
-      <Form input={input} setInput={setInput}/>
+      <Form input={input} setInput={setInput} addTodo={addTodo} />
       {/* Todo List*/}
       {/* Key*/}
       {/* Author*/}
